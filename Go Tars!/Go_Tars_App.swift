82e50c37c2807c343504f59ff3d62760ebf5986e
Jari Polm <jpolm@rollins.edu>
2022-01-28 22:34:22 -0500

    Initial Commit

---
+++ Go Tars!/Go_Tars_App.swift
@@ -0,0 +1,17 @@
+//
+//  Go_Tars_App.swift
+//  Go Tars!
+//
+//  Created by Jari Polm on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Go_Tars_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
